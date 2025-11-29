db680f7b55462d3080bd9a5059c656461a4d61a0
osscar <osscar.unheard025@passmail.net>
2025-11-29 11:39:41 +0100

    jj: add jjui

--- machines/simon-desktop/home/cli/jujutsu.nix
+++ machines/simon-desktop/home/cli/jujutsu.nix
@@ -7,48 +7,51 @@ in
   imports = [
   ];
 
-  programs.jujutsu = {
-    enable = true;
-    settings = {
-      user = {
-        inherit email name;
-      };
-      ui = {
-        default-command = "log"; # or status. log is more verboses
-        diff-editor = ":builtin";
-        diff-formatter = ":git";
-        #merge-editor = "meld";
-        editor = "nvim";
-        graph = {
-          style = "curved";
+  programs = {
+    jjui.enable = true;
+    jujutsu = {
+      enable = true;
+      settings = {
+        user = {
+          inherit email name;
         };
-        pager = "delta";
-        paginate = "auto";
-      };
-      git = {
-        auto-local-bookmark = true;
-        sign-on-push = true;
-      };
-      signing = {
-        backend = "ssh";
-        behavior = "drop";
-        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID3AsDe157avF+iFa1TavZHwjDpugyePDqJ6gaRNzGIA";
-        #key = "1394DCC0EC169ED4";
-      };
-      snapshot = {
-        max-new-file-size = 16000000; # ~16mb
-      };
-      init = {
-        default_branch = "main";
-      };
-      merge-tools = {
-        difft = {
-          program = "difft";
-          diff-args = [
-            "--color=always"
-            "$left"
-            "$right"
-          ];
+        ui = {
+          default-command = "log"; # or status. log is more verboses
+          diff-editor = ":builtin";
+          diff-formatter = ":git";
+          #merge-editor = "meld";
+          editor = "nvim";
+          graph = {
+            style = "curved";
+          };
+          pager = "delta";
+          paginate = "auto";
+        };
+        git = {
+          auto-local-bookmark = true;
+          sign-on-push = true;
+        };
+        signing = {
+          backend = "ssh";
+          behavior = "drop";
+          key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID3AsDe157avF+iFa1TavZHwjDpugyePDqJ6gaRNzGIA";
+          #key = "1394DCC0EC169ED4";
+        };
+        snapshot = {
+          max-new-file-size = 16000000; # ~16mb
+        };
+        init = {
+          default_branch = "main";
+        };
+        merge-tools = {
+          difft = {
+            program = "difft";
+            diff-args = [
+              "--color=always"
+              "$left"
+              "$right"
+            ];
+          };
         };
       };
     };
