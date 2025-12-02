1b9fa3cf106af7acfdc7f12351e84653f08827da
osscar <osscar.unheard025@passmail.net>
2025-12-02 14:29:06 +0100

    cleanup old files

--- archive/hzc-k8s-ctrl2/k3s-control-2.nix
+++
@@ -1,9 +0,0 @@
-_: {
-  k3s.controlPlane = {
-    enable = true;
-    nodeName = "k3s-control-2";
-    nodeIp = "10.0.0.3";
-    serverAddr = "https://10.0.0.2:6443";
-    tlsSans = [ "k3s.simonoscar.me" ];
-  };
-}
