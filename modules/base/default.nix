6f438d7a3aedff9a36d26dcbd28352485605012c
osscar <osscar.unheard025@passmail.net>
2025-12-13 23:50:11 +0100

    yggdrasil: initial setup

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -1,4 +1,17 @@
-{ mylib, ... }:
+{ lib, mylib, ... }:
 {
-  imports = mylib.scanPaths ./. { };
+  imports = mylib.scanPaths ./. { } ++ [
+    ../borgbackup
+  ];
+
+  # disable yggdrasil multicast - use explicit peers only (security)
+  services.yggdrasil.settings.MulticastInterfaces = lib.mkForce [ ];
+  services.yggdrasil.openMulticastPort = lib.mkForce false;
+
+  # only allow peering from known machines
+  services.yggdrasil.settings.AllowedPublicKeys = [
+    "48366f4c85cd25f6d6e514928965559a0ab2e161b0ed9caf4e4d289f4ca71522" # simon-desktop
+    "db01e0e96a4969bfa69a53c56646b0c3d3bbcc620a086f0f96174e915e6d16a5" # hm-nixbox
+    "204f30349bc1818988ff47245a296be0d30d001af6ed7ad8c93e3f5af78dafa9" # hzc-pango
+  ];
 }
--- modules/server/network.nix
+++
@@ -1,5 +0,0 @@
-{ lib, ... }:
-{
-  # servers: no networkmanager, no ipv6
-  networking.enableIPv6 = lib.mkForce false;
-}
