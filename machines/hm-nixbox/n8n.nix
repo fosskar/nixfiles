1db3486c17deacd69c53f01894d5720b080dc31c
osscar <osscar.unheard025@passmail.net>
2025-12-16 16:58:14 +0100

    flake: update

--- machines/hm-nixbox/n8n.nix
+++ machines/hm-nixbox/n8n.nix
@@ -1,6 +1,8 @@
 _: {
   services.n8n = {
-    enable = true;
+    # temporarily disabled - hash mismatch in nixpkgs
+    # see: https://github.com/NixOS/nixpkgs/issues (search n8n hash)
+    enable = false;
     openFirewall = false;
     environment.N8N_PORT = 5679;
   };
