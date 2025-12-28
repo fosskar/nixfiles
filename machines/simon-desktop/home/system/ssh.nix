cc027303812b3f22300ff609fdfe30096ffa619c
osscar <osscar.unheard025@passmail.net>
2025-12-28 23:05:48 +0100

    simon-desktop: add ssh config

--- machines/simon-desktop/home/system/ssh.nix
+++ machines/simon-desktop/home/system/ssh.nix
@@ -4,9 +4,17 @@ _: {
     enableDefaultConfig = false;
     matchBlocks = {
       "*" = {
+        user = "root";
         addKeysToAgent = "no";
+        controlMaster = "auto";
+        controlPath = "/tmp/ssh-%u-%r@%h:%p";
+        controlPersist = "10m";
+        serverAliveInterval = 60;
+        serverAliveCountMax = 3;
+        compression = true;
         extraOptions = {
           UpdateHostKeys = "yes";
+          StrictHostKeyChecking = "accept-new";
         };
       };
     };
