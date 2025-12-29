a24659c47bf9ef4fd8c403e20544db247060fe35
osscar <osscar.unheard025@passmail.net>
2025-12-29 16:46:18 +0100

    hm-nixbox: remove n8n

--- machines/hm-nixbox/n8n.nix
+++
@@ -1,9 +0,0 @@
-_: {
-  services.n8n = {
-    # temporarily disabled - hash mismatch in nixpkgs
-    # see: https://github.com/NixOS/nixpkgs/issues (search n8n hash)
-    enable = false;
-    openFirewall = false;
-    environment.N8N_PORT = 5679;
-  };
-}
