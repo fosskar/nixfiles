080c7b2c9c0ce5e05b8a754c0ba7eacc45dd07f8
osscar <osscar.unheard025@passmail.net>
2025-12-29 18:46:31 +0100

    hm-nixbox: add nginx vhost module

---
+++ modules/nginx/default.nix
@@ -0,0 +1,51 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.nixfiles.nginx;
+  acmeDomain = config.nixfiles.acme.domain;
+
+  vhostModule = lib.types.submodule {
+    options = {
+      port = lib.mkOption {
+        type = lib.types.port;
+        description = "backend port to proxy to";
+      };
+      websockets = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = "enable websocket proxying";
+      };
+      extraConfig = lib.mkOption {
+        type = lib.types.lines;
+        default = "";
+        description = "extra nginx config for this vhost";
+      };
+    };
+  };
+in
+{
+  options.nixfiles.nginx.vhosts = lib.mkOption {
+    type = lib.types.attrsOf vhostModule;
+    default = { };
+    description = "simplified vhost definitions";
+  };
+
+  config = lib.mkIf (cfg.vhosts != { }) {
+    services.nginx.virtualHosts = lib.mapAttrs' (name: vhost: {
+      name = "${name}.${acmeDomain}";
+      value = {
+        useACMEHost = acmeDomain;
+        forceSSL = true;
+        inherit (vhost) extraConfig;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:${toString vhost.port}";
+          recommendedProxySettings = true;
+          proxyWebsockets = vhost.websockets;
+        };
+      };
+    }) cfg.vhosts;
+  };
+}
