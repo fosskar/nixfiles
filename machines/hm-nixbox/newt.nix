8ed13558f1167826b2143e6a2176099a0f477641
osscar <osscar.unheard025@passmail.net>
2025-12-30 13:15:58 +0100

    hm-nixbox/newt: fix endpoint url scheme

--- machines/hm-nixbox/newt.nix
+++ machines/hm-nixbox/newt.nix
@@ -8,5 +8,6 @@
     enable = true;
     package = pkgs.custom.fosrl-newt;
     environmentFile = config.sops.secrets."newt.env".path;
+    settings.endpoint = "https://pangolin.fosskar.eu";
   };
 }
