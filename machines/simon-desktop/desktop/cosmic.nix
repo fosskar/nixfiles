c823ed64a485eea59debc065c12800fdcb19705d
fosskar <fosskar.unheard025@passmail.net>
2026-01-01 17:32:40 +0100

    cosmic: exclude nm-applet

--- machines/simon-desktop/desktop/cosmic.nix
+++ machines/simon-desktop/desktop/cosmic.nix
@@ -1,4 +1,5 @@
-_: {
+{ pkgs, ... }:
+{
   services = {
     displayManager.cosmic-greeter = {
       enable = false; # yeah its ugly
@@ -7,4 +8,7 @@ _: {
       enable = true;
     };
   };
+
+  # nm-applet not needed - cosmic has its own network indicator
+  environment.cosmic.excludePackages = [ pkgs.networkmanagerapplet ];
 }
