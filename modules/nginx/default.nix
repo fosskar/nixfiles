25b1fb0ab42c3c1a119115dc67b88684cc060b76
fosskar <fosskar.educated493@passmail.net>
2026-02-02 19:37:37 +0100

    nginx: add security headers + recommended settings

--- modules/nginx/default.nix
+++ modules/nginx/default.nix
@@ -34,27 +34,40 @@ in
   };
 
   config = lib.mkIf (cfg.vhosts != { }) {
-    services.nginx.virtualHosts = {
-      # reject requests to unknown subdomains
-      "_" = {
-        default = true;
-        useACMEHost = acmeDomain;
-        forceSSL = true;
-        locations."/".return = "444";
-      };
-    }
-    // lib.mapAttrs' (name: vhost: {
-      name = "${name}.${acmeDomain}";
-      value = {
-        useACMEHost = acmeDomain;
-        forceSSL = true;
-        inherit (vhost) extraConfig;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:${toString vhost.port}";
-          recommendedProxySettings = true;
-          proxyWebsockets = vhost.websockets;
+    services.nginx = {
+      recommendedTlsSettings = true;
+      recommendedOptimisation = true;
+      recommendedGzipSettings = true;
+
+      appendHttpConfig = ''
+        add_header Strict-Transport-Security "max-age=31536000; includeSubDomains" always;
+        add_header X-Content-Type-Options "nosniff" always;
+        add_header X-Frame-Options "SAMEORIGIN" always;
+        add_header Referrer-Policy "strict-origin-when-cross-origin" always;
+      '';
+
+      virtualHosts = {
+        # reject requests to unknown subdomains
+        "_" = {
+          default = true;
+          useACMEHost = acmeDomain;
+          forceSSL = true;
+          locations."/".return = "444";
         };
-      };
-    }) cfg.vhosts;
+      }
+      // lib.mapAttrs' (name: vhost: {
+        name = "${name}.${acmeDomain}";
+        value = {
+          useACMEHost = acmeDomain;
+          forceSSL = true;
+          inherit (vhost) extraConfig;
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:${toString vhost.port}";
+            recommendedProxySettings = true;
+            proxyWebsockets = vhost.websockets;
+          };
+        };
+      }) cfg.vhosts;
+    };
   };
 }
