751853143b659d27f4f18404a479cbef55691b0d
fosskar <fosskar.educated493@passmail.net>
2026-02-26 18:15:05 +0100

    patch clan-cli to support SOPS_AGE_KEY_CMD

--- shells/flake-module.nix
+++ shells/flake-module.nix
@@ -9,13 +9,19 @@ _: {
     let
       mkScript = name: text: pkgs.writeShellScriptBin name text;
 
+      # patch clan-cli to support SOPS_AGE_KEY_CMD
+      # https://git.clan.lol/clan/clan-core/issues/6799
+      clan-cli-patched = inputs'.clan-core.packages.clan-cli.overrideAttrs (old: {
+        patches = (old.patches or [ ]) ++ [ ./clan-cli-sops-age-key-cmd.patch ];
+      });
+
       # until it not only a cli flag https://git.clan.lol/clan/clan-core/issues/4624
       scripts = [
         (mkScript "clan" ''
           if [ "$1" = "machines" ] && [ "$2" = "update" ]; then
-            ${inputs'.clan-core.packages.clan-cli}/bin/clan machines update --build-host localhost "''${@:3}"
+            ${clan-cli-patched}/bin/clan machines update --build-host localhost "''${@:3}"
           else
-            ${inputs'.clan-core.packages.clan-cli}/bin/clan "$@"
+            ${clan-cli-patched}/bin/clan "$@"
           fi
         '')
       ];
