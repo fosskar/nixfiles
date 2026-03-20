3248aa71197577af0809c9f8b5895e096c98c9a5
fosskar <fosskar.educated493@passmail.net>
2026-03-20 16:58:08 +0100

    fix nix-index-database hm module path

--- users/simon/cli/nix-index.nix
+++ users/simon/cli/nix-index.nix
@@ -8,7 +8,7 @@
   ...
 }:
 {
-  imports = [ inputs.nix-index-database.hmModules.nix-index ];
+  imports = [ inputs.nix-index-database.homeModules.nix-index ];
 
   programs.nix-index-database.comma.enable = true;
 }
