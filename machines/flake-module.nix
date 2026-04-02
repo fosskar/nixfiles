4fa5633d4655f5a7ec899748d6949059e61f3985
fosskar <fosskar.educated493@passmail.net>
2026-04-02 17:02:03 +0200

    move ncps to nixworker, use tags.all for clients, clean up caches

--- machines/flake-module.nix
+++ machines/flake-module.nix
@@ -413,30 +413,21 @@ in
             input = "clan-core";
           };
           roles = {
-            server.machines."nixbox".settings = {
+            server.machines."nixworker".settings = {
               caches = [
                 "https://cache.nixos.org"
                 "https://nix-community.cachix.org"
-                "https://hyprland.cachix.org"
-                "https://cosmic.cachix.org"
-                "https://nixpkgs-unfree.cachix.org"
                 "https://nix-gaming.cachix.org"
+                "https://numtide.cachix.org"
               ];
               publicKeys = [
                 "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
                 "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-                "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-                "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
-                "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
                 "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+                "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
               ];
             };
-            client.machines = {
-              simon-desktop.settings = { };
-              lpt-titan.settings = { };
-              clawbox.settings = { };
-              gateway.settings = { };
-            };
+            client.tags.all = { };
           };
         };
 
