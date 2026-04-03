484650d7575e12da4fffa5b25e2709ed92ac7087
fosskar <fosskar.educated493@passmail.net>
2026-04-03 10:42:40 +0200

    noctalia: add colorscheme, light mode, tray/plugin tweaks

--- users/simon/desktop/shells/noctalia/default.nix
+++ users/simon/desktop/shells/noctalia/default.nix
@@ -12,32 +12,59 @@
       t = config.theme;
     in
     {
-      xdg.configFile."noctalia/pam/password.conf".text = ''
-        auth sufficient pam_fprintd.so max-tries=1
-        auth required pam_unix.so
-      '';
+      #xdg.configFile."noctalia/pam/password.conf".text = ''
+      #  auth sufficient pam_fprintd.so max-tries=1
+      #  auth required pam_unix.so
+      #'';
+
+      xdg.configFile."noctalia/colorschemes/grey-teal/grey-teal.json".text = builtins.toJSON {
+        dark = {
+          mPrimary = t.primary;
+          mOnPrimary = t.bg;
+          mSecondary = t.secondary;
+          mOnSecondary = t.bg;
+          mTertiary = t.info;
+          mOnTertiary = t.bg;
+          mError = t.error;
+          mOnError = t.bg;
+          mSurface = t.bg;
+          mOnSurface = t.fg;
+          mHover = t.primary;
+          mOnHover = t.bg;
+          mSurfaceVariant = t.bgLight;
+          mOnSurfaceVariant = t.fgMuted;
+          mOutline = t.fgDim;
+          mShadow = t.bg;
+        };
+        light = {
+          mPrimary = t.light.primary;
+          mOnPrimary = t.light.bg;
+          mSecondary = t.secondary;
+          mOnSecondary = t.light.bg;
+          mTertiary = t.info;
+          mOnTertiary = t.light.bg;
+          mError = t.light.error;
+          mOnError = t.light.bg;
+          mSurface = t.light.bg;
+          mOnSurface = t.light.fg;
+          mHover = t.light.primary;
+          mOnHover = t.light.bg;
+          mSurfaceVariant = t.light.bgDark;
+          mOnSurfaceVariant = t.light.fgMuted;
+          mOutline = t.light.outline;
+          mShadow = t.light.bgDarkest;
+        };
+      };
 
       programs.noctalia-shell = {
         enable = true;
         systemd.enable = true;
-        colors = {
-          mPrimary = t.primary;
-          mSecondary = t.secondary;
-          mTertiary = t.info;
-          mError = t.error;
-          mSurface = t.bg;
-          mSurfaceVariant = t.bgLight;
-          mOnPrimary = "#FFFFFF";
-          mOnSecondary = "#FFFFFF";
-          mOnTertiary = "#FFFFFF";
-          mOnError = "#FFFFFF";
-          mOnSurface = t.fg;
-          mOnSurfaceVariant = t.fgMuted;
-          mOutline = t.fgDim;
-          mShadow = "#000000";
-        };
-        # plugins managed via noctalia GUI
+
         settings = {
+          colorSchemes = {
+            predefinedScheme = "grey-teal";
+            darkMode = true;
+          };
           appLauncher = {
             terminalCommand = "ghostty -e";
           };
@@ -69,6 +96,11 @@
               right = [
                 {
                   id = "Tray";
+                  drawerEnabled = false;
+                  hidePassive = true;
+                }
+                {
+                  id = "plugin:model-usage";
                 }
                 {
                   id = "Microphone";
@@ -121,6 +153,7 @@
           general = {
             showScreenCorners = true;
             forceBlackScreenCorners = true;
+            compactLockScreen = true;
           };
 
           osd.location = "right";
