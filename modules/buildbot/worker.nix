71c061167e7ea1ac73d713cb46c6bd367dd7db35
fosskar <fosskar.educated493@passmail.net>
2026-04-07 09:30:23 +0200

    provision ssh key for buildbot-worker so eval can fetch private flake inputs

--- modules/buildbot/worker.nix
+++ modules/buildbot/worker.nix
@@ -2,6 +2,7 @@
   config,
   inputs,
   lib,
+  pkgs,
   ...
 }:
 let
@@ -25,6 +26,31 @@ in
       enable = true;
       inherit (cfg) masterUrl;
       workerPasswordFile = config.clan.core.vars.generators.buildbot-master.files."worker-password".path;
+    };
+
+    # ssh key for fetching private flake inputs (e.g. nixsecrets) during eval
+    clan.core.vars.generators.buildbot-worker-ssh = {
+      files."id_ed25519" = {
+        owner = "buildbot-worker";
+        group = "buildbot-worker";
+        mode = "0600";
+      };
+      files."id_ed25519.pub".secret = false;
+      runtimeInputs = [ pkgs.openssh ];
+      script = ''
+        ssh-keygen -t ed25519 -N "" -C buildbot-worker -f "$out/id_ed25519"
+      '';
+    };
+
+    systemd.tmpfiles.settings."10-buildbot-worker-ssh" = {
+      "/var/lib/buildbot-worker/.ssh".d = {
+        user = "buildbot-worker";
+        group = "buildbot-worker";
+        mode = "0700";
+      };
+      "/var/lib/buildbot-worker/.ssh/id_ed25519"."L+" = {
+        argument = config.clan.core.vars.generators.buildbot-worker-ssh.files."id_ed25519".path;
+      };
     };
   };
 }
