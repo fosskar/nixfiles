34fffacad19ece161e5d66e341b8e598243507bd
fosskar <fosskar.educated493@passmail.net>
2026-04-07 12:09:18 +0200

    expose wiki via static-web-server for netbird reverse proxy

--- machines/nixbox/wiki.nix
+++ machines/nixbox/wiki.nix
@@ -1,6 +1,15 @@
 { inputs, ... }:
+let
+  port = 8086;
+in
 {
-  nixfiles.caddy.vhosts.wiki = {
+  nixfiles.caddy.vhosts.fosskar = {
+    inherit port;
+  };
+
+  services.static-web-server = {
+    enable = true;
+    listen = "0.0.0.0:${toString port}";
     root = inputs.wiki.packages.x86_64-linux.default;
   };
 }
