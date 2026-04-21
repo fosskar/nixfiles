82d209986c00b74b31f05b38d9d29a6d42f34759
fosskar <fosskar.educated493@passmail.net>
2026-04-21 15:45:35 +0200

    packages: bump netbird 0.68.3 -> 0.69.0, dashboard 2.36.0 -> 2.37.0

--- packages/netbird-dashboard/default.nix
+++ packages/netbird-dashboard/default.nix
@@ -6,16 +6,16 @@
 }:
 buildNpmPackage rec {
   pname = "netbird-dashboard";
-  version = "2.36.0";
+  version = "2.37.0";
 
   src = fetchFromGitHub {
     owner = "netbirdio";
     repo = "dashboard";
     rev = "v${version}";
-    hash = "sha256-VsecD83dz6U6jEaGIxv7M9ePzbTPCXeffSoyyBr2Vh4=";
+    hash = "sha256-kEQ8bJgO+PRHElrU9Zs03qgiFwHE7J3AHBkhmAS4gro=";
   };
 
-  npmDepsHash = "sha256-ljko66NYBgwyvgIbqnexfbSaILNf/74qrNXZUsHT8/o=";
+  npmDepsHash = "sha256-Ze+1r5Uh+wdm3MuVr93oS2itodx9Zdv+JYO6Uji1saw=";
   npmFlags = [ "--legacy-peer-deps" ];
 
   # auth config is baked in at build time via string replacement in the JS bundle.
