f3c5ec899c37f27d37f2cecaf5f84ceeb6697d6b
fosskar <fosskar@noreply.codeberg.org>
2026-04-24 22:50:15 +0200

    update t3code (#292)

--- packages/t3code/default.nix
+++ packages/t3code/default.nix
@@ -7,11 +7,11 @@
 }:
 let
   pname = "t3code";
-  version = "0.0.20";
+  version = "0.0.21";
 
   src = fetchurl {
     url = "https://github.com/pingdotgg/t3code/releases/download/v${version}/T3-Code-${version}-x86_64.AppImage";
-    hash = "sha256-glYnF8UA5s4rrpUJuvk4HlQtyMikbckIkmMIhnJugO4=";
+    hash = "sha256-eQCfskpl+JJOyaYY7ogYCi0ZCuWNRcEpseWMniS/LCQ=";
   };
 
   appimageContents = appimageTools.extractType2 { inherit pname version src; };
