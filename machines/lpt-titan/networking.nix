6d8ae68a9c9dbc7ff24bff7a8d3e3fd29c24e317
fosskar <fosskar.educated493@passmail.net>
2026-04-25 17:58:35 +0200

    set wifi gateway explicitly

--- machines/lpt-titan/networking.nix
+++ machines/lpt-titan/networking.nix
@@ -3,6 +3,7 @@
     ipv4 = {
       method = "manual";
       address1 = "192.168.10.150/24,192.168.10.1";
+      gateway = "192.168.10.1";
       dns = "192.168.10.1;";
     };
     ipv6.method = "auto";
