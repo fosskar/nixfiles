d7311988a5045c53f4a888e3e76ad74302d8f76e
fosskar <fosskar.educated493@passmail.net>
2026-04-27 16:17:52 +0200

    scrub btrfs monthly to catch silent bitrot, mirroring zfs default

--- modules/nixos/filesystems/btrfs.nix
+++ modules/nixos/filesystems/btrfs.nix
@@ -4,5 +4,12 @@
       supportedFilesystems = [ "btrfs" ];
       initrd.supportedFilesystems = [ "btrfs" ];
     };
+
+    # monthly scrub catches silent bitrot/checksum errors. mirrors the
+    # services.zfs.autoScrub default in modules/nixos/filesystems/zfs.nix.
+    services.btrfs.autoScrub = {
+      enable = true;
+      interval = "monthly";
+    };
   };
 }
