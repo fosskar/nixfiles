f70a300aefd46fd5d7152a05be94fdef2276ebda
fosskar <fosskar.educated493@passmail.net>
2026-04-29 16:59:51 +0200

    manage radicle via home-manager programs/services modules
    switch from raw radicle-node package install to programs.radicle and
    services.radicle.node (lazy mode). radicle-desktop stays in
    home.packages.

--- users/simon/cli/radicle.nix
+++ users/simon/cli/radicle.nix
@@ -1,7 +1,15 @@
 { pkgs, ... }:
 {
+  programs.radicle = {
+    enable = true;
+  };
+
+  services.radicle.node = {
+    enable = true;
+    lazy.enable = true;
+  };
+
   home.packages = with pkgs; [
-    radicle-node
     radicle-desktop
   ];
 }
