bf8abc6f3992bfc294449bfaf8409f78b0a5e7b5
fosskar <fosskar@noreply.codeberg.org>
2026-05-01 10:06:49 +0200

    update netbird-proxy (#384)

--- packages/netbird-proxy/default.nix
+++ packages/netbird-proxy/default.nix
@@ -6,16 +6,16 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "netbird-proxy";
-  version = "0.70.0";
+  version = "0.70.4";
 
   src = fetchFromGitHub {
     owner = "netbirdio";
     repo = "netbird";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-M0rOcIjtp/FicrdMRyMVRjJaAHAU6frgNqYqadd7jlg=";
+    hash = "sha256-tfScscRllUlV1V6D66rfT6JEsReDQfVGryVzNebm0vg=";
   };
 
-  vendorHash = "sha256-NK2+FpI4SJtxpFkRRMUmhPDg+adIvJWqYWyumP5ViN4=";
+  vendorHash = "sha256-IRV1GxdUKgan0GwmBg9acpl7plW01CtEO2FrKrlDdeE=";
 
   subPackages = [ "proxy/cmd/proxy" ];
 
