f2eb6189815baa4a5cc61c03317327e4f9831205
fosskar <fosskar.educated493@passmail.net>
2026-05-04 12:56:59 +0200

    ignore noisy beszel sensor

--- machines/flake-module.nix
+++ machines/flake-module.nix
@@ -397,7 +397,7 @@ in
             server.machines."nixbox" = { };
             client.tags.server = { };
             client.machines."nixbox".settings = {
-              sensors = "-nct6798_cputin,-nct6798_auxtin0,-nct6798_auxtin2,-nct6798_auxtin4";
+              sensors = "-nct6798_cputin,-nct6798_auxtin0,-nct6798_auxtin2,-nct6798_auxtin4,-bnxt_en";
               filesystem = "/persist";
               extraFilesystems = "/__Root,/nix__Nix,/boot__Boot,/boot-fallback__BootFallback,/tank__Tank,/tank/apps__Apps,/tank/media__Media,/tank/shares__Shares,/tank/backup__Backup";
               smartDevices = "/dev/nvme0,/dev/nvme1,/dev/sda,/dev/sdb,/dev/sdc,/dev/sdd,/dev/sde,/dev/sdf,/dev/sdg";
