4f90b34c41c696b1cadcd1ed5623447ff6532947
fosskar <fosskar@noreply.codeberg.org>
2026-05-08 06:11:18 +0200

    update voquill (#491)

--- packages/voquill/default.nix
+++ packages/voquill/default.nix
@@ -44,7 +44,7 @@
 }:
 let
   pname = "voquill";
-  version = "0.0.620";
+  version = "0.0.641";
 
   runtimeDeps = [
     # tauri/webkit runtime deps
@@ -91,7 +91,7 @@ stdenv.mkDerivation {
   # use pre-built binary from github releases
   src = fetchurl {
     url = "https://github.com/josiahsrc/voquill/releases/download/desktop-v${version}/voquill-desktop_${version}_amd64.deb";
-    hash = "sha256-oFniufob0ujiwmF+UoDEYo4gGToTSa2GOOuXbdA7bzw=";
+    hash = "sha256-Jlz39OygSRej3Avc9NIxQYo+W2A/Hx9CuXCSzrlVeVc=";
   };
 
   nativeBuildInputs = [
