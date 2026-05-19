df1dafa6d76cce9d122a6c4b41dfe6ce7b54067f
fosskar <fosskar.educated493@passmail.net>
2026-05-19 10:33:17 +0200

    ssh: use settings for default host block

--- users/simon/system/ssh.nix
+++ users/simon/system/ssh.nix
@@ -2,26 +2,22 @@ _: {
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks = {
-      "*" = {
-        user = "root";
-        addKeysToAgent = "no";
-        controlMaster = "auto";
-        controlPath = "/tmp/ssh-%u-%r@%h:%p";
-        controlPersist = "10m";
-        serverAliveInterval = 60;
-        serverAliveCountMax = 3;
-        compression = true;
-        extraOptions = {
-          # FIXME: Work around gpg-agent smartcard signing failures with hostbound pubkey auth.
-          PubkeyAuthentication = "unbound";
-          UpdateHostKeys = "yes";
-          StrictHostKeyChecking = "accept-new";
-          # ensure ssh finds gpg-agent even when SSH_AUTH_SOCK is stripped
-          # (e.g. nixos-rebuild-ng env sanitization, nixpkgs#493085)
-          IdentityAgent = "/run/user/1000/gnupg/S.gpg-agent.ssh";
-        };
-      };
+    settings."*" = {
+      User = "root";
+      AddKeysToAgent = "no";
+      ControlMaster = "auto";
+      ControlPath = "/tmp/ssh-%u-%r@%h:%p";
+      ControlPersist = "10m";
+      ServerAliveInterval = 60;
+      ServerAliveCountMax = 3;
+      Compression = true;
+      # FIXME: Work around gpg-agent smartcard signing failures with hostbound pubkey auth.
+      PubkeyAuthentication = "unbound";
+      UpdateHostKeys = "yes";
+      StrictHostKeyChecking = "accept-new";
+      # ensure ssh finds gpg-agent even when SSH_AUTH_SOCK is stripped
+      # (e.g. nixos-rebuild-ng env sanitization, nixpkgs#493085)
+      IdentityAgent = "/run/user/1000/gnupg/S.gpg-agent.ssh";
     };
   };
 }
