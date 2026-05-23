e44552fe36f8563a3010e34b634f571352f25f4f
fosskar <fosskar.educated493@passmail.net>
2026-05-23 08:55:57 +0200

    journaldUpload: order upload after log transports

--- modules/nixos/services/monitoring/journald-upload.nix
+++ modules/nixos/services/monitoring/journald-upload.nix
@@ -1,13 +1,25 @@
 {
   flake.modules.nixos.journaldUpload =
-    { config, lib, ... }:
+    {
+      config,
+      lib,
+      ...
+    }:
     {
       config = lib.mkIf config.services.journald.upload.enable {
         services.journald.upload.settings.Upload.NetworkTimeoutSec = lib.mkDefault "30s";
 
         systemd.services.systemd-journal-upload = {
-          wants = [ "network-online.target" ];
-          after = [ "network-online.target" ];
+          wants = [
+            "network-online.target"
+            "yggdrasil.service"
+          ]
+          ++ lib.optional config.services.victorialogs.enable "victorialogs.service";
+          after = [
+            "network-online.target"
+            "yggdrasil.service"
+          ]
+          ++ lib.optional config.services.victorialogs.enable "victorialogs.service";
         };
       };
     };
