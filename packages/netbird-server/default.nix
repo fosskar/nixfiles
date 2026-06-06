617f42c4c3b59c20ff1be2f2f151977a0d1b59ba
fosskar <fosskar@noreply.codeberg.org>
2026-06-06 06:50:02 +0200

    update netbird-server (#856)

--- packages/netbird-server/default.nix
+++ packages/netbird-server/default.nix
@@ -6,16 +6,16 @@
 }:
 buildGoModule (finalAttrs: {
   pname = "netbird-server";
-  version = "0.71.4";
+  version = "0.72.1";
 
   src = fetchFromGitHub {
     owner = "netbirdio";
     repo = "netbird";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-e/fe4wEjz7apA5RZ4nGIaunp0+5NVH4yMHK/l/MfcWI=";
+    hash = "sha256-f78zf6uihFyz4H8/ZXk2rfO8nln+HMNIV5/yQHRpdQI=";
   };
 
-  vendorHash = "sha256-NeZuj9o2yu5di+6jbNqCnAw0fI55GA5Otmr77c08QFc=";
+  vendorHash = "sha256-gxSf2X7IsZecvfM2xGE5Y1jZgvaKaRG780MRkZKFkHg=";
 
   subPackages = [ "combined" ];
 
