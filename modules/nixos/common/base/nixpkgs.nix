76e1c21e54bfe360082194dec99f551acc1fdc16
fosskar <fosskar.educated493@passmail.net>
2026-06-12 07:59:55 +0000

    nixpkgs: drop config options set to their defaults

--- modules/nixos/common/base/nixpkgs.nix
+++ modules/nixos/common/base/nixpkgs.nix
@@ -5,18 +5,7 @@
       nixpkgs = {
         overlays = [ self.overlays.default ];
 
-        config = {
-          allowBroken = false;
-          allowUnsupportedSystem = true;
-          allowDeprecatedx86_64Darwin = true;
-          allowUnfree = true;
-          permittedInsecurePackages = [ ];
-          allowAliases = true;
-          # true causes full-closure mass rebuild
-          enableParallelBuildingByDefault = false;
-          # see pkgs/stdenv/generic/check-meta.nix for valid values
-          showDerivationWarnings = [ ];
-        };
+        config.allowUnfree = true;
       };
     };
 }
