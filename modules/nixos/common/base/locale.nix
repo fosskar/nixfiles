1e33812685f8cc34d60a30730fbaca6e7f5b0b41
fosskar <fosskar.educated493@passmail.net>
2026-06-12 07:59:55 +0000

    locale: drop LC_ALL override
    LC_ALL overrides every category and forced german messages despite
    LANG/LANGUAGE being english.

--- modules/nixos/common/base/locale.nix
+++ modules/nixos/common/base/locale.nix
@@ -5,7 +5,7 @@
       extraLocaleSettings = {
         LANGUAGE = "en_US.UTF-8";
         LANG = "en_US.UTF-8";
-        LC_ALL = "de_DE.UTF-8";
+        # no LC_ALL: it overrides every category and would force german messages
         LC_ADDRESS = "de_DE.UTF-8";
         LC_IDENTIFICATION = "de_DE.UTF-8";
         LC_MEASUREMENT = "de_DE.UTF-8";
