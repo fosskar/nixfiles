4281c3f60087d332fe9d83410293923ba53388ab
fosskar <fosskar.educated493@passmail.net>
2026-07-06 20:11:09 +0200

    effects: replace forgejo update-flake-inputs workflow with scheduled effect
    Run update-flake-inputs-gitea as a hercules onSchedule effect instead of
    the forgejo actions workflow. The effect needs nix in its environment to
    run the tool; keep the workflow file around disabled.

--- modules/flake-parts/effects.nix
+++ modules/flake-parts/effects.nix
@@ -18,6 +18,7 @@ let
           pkgs.cacert
           pkgs.git
           pkgs.jq
+          pkgs.nix
         ];
         secretsMap = builtins.toJSON {
           git.type = "GitToken";
@@ -103,6 +104,27 @@ in
         minute = 0;
       };
       outputs.effects.renovate = renovate;
+    };
+    onSchedule.update-flake-inputs = {
+      when = {
+        hour = 4;
+        minute = 0;
+      };
+      outputs.effects.update-flake-inputs = mkRepoEffect "update-flake-inputs" ''
+        export GITEA_TOKEN="$token"
+        export NIX_CONFIG="$NIX_CONFIG
+        access-tokens = github.com=$GITHUB_TOKEN"
+
+        nix run "github:Mic92/update-flake-inputs-gitea" -- \
+          --gitea-url "https://${forgeHost}" \
+          --gitea-repository "${repo}" \
+          --base-branch "main" \
+          --git-author-name "nixbot" \
+          --git-author-email "nixbot@noreply.codeberg.org" \
+          --git-committer-name "nixbot" \
+          --git-committer-email "nixbot@noreply.codeberg.org" \
+          --auto-merge
+      '';
     };
   };
 }
