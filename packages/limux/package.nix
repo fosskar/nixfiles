2e7c2e63b436f6c405c202901e9b470fb8d11fee
fosskar <fosskar@noreply.codeberg.org>
2026-07-06 20:11:04 +0200

    limux: 0.1.19 -> 0.1.21 (#1167)
    Changelog: https://github.com/am-will/limux/releases/tag/v0.1.19
    Co-authored-by: nixbot <nixbot@nx3.eu>
    Reviewed-on: https://codeberg.org/fosskar/nixfiles/pulls/1167

--- packages/limux/package.nix
+++ packages/limux/package.nix
@@ -13,11 +13,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "limux";
-  version = "0.1.19";
+  version = "0.1.21";
 
   src = fetchurl {
     url = "https://github.com/am-will/limux/releases/download/v${finalAttrs.version}/limux-${finalAttrs.version}-linux-x86_64.tar.gz";
-    hash = "sha256-94/s5Iugdf3vbiwwVviGhVe5tSBnDi4Cbsib3yzeNNg=";
+    hash = "sha256-vt+hsiQWP5IxDQ/htlEKMOt/seppFBkoyz2AwieFBlE=";
   };
 
   nativeBuildInputs = [
