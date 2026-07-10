ad90fb3f7b3c3d93b1b7fa678ff0941af6f53caf
fosskar <117449098+fosskar@users.noreply.github.com>
2026-07-10 12:35:47 +0200

    flake-parts: drop unused self argument from lib wiring

--- modules/flake-parts/lib.nix
+++ modules/flake-parts/lib.nix
@@ -1,6 +1,6 @@
-{ lib, self, ... }:
+{ lib, ... }:
 {
   # nflib helper set (defined in lib/), exposed as a flake output + propagated
   # into modules via clan specialArgs (nflib = config.flake.lib).
-  flake.lib = import ../../lib { inherit lib self; };
+  flake.lib = import ../../lib { inherit lib; };
 }
