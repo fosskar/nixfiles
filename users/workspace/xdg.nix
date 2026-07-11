9514e3bf8b6df2c0f87d612737275c2587a8af24
fosskar <117449098+fosskar@users.noreply.github.com>
2026-07-11 10:14:02 +0200

    workspace: keep exporting XDG user dir session variables
    home-manager flipped the setSessionVariables default to false.

--- users/workspace/xdg.nix
+++ users/workspace/xdg.nix
@@ -2,6 +2,8 @@
   xdg.userDirs = {
     enable = true;
     createDirectories = true;
+    # hm default flipped to false; keep exporting XDG_*_DIR like before
+    setSessionVariables = true;
     # only the projects dir is wanted for this user
     desktop = null;
     music = null;
