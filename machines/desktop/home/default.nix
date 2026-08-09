178bfcc3832e88c3cda8dcc8c732aec703ea2fc3
fosskar <117449098+fosskar@users.noreply.github.com>
2026-08-09 07:08:50 +0000

    noctalia: add lockscreen login box widgets

--- machines/desktop/home/default.nix
+++ machines/desktop/home/default.nix
@@ -11,5 +11,33 @@
       self.modules.homeManager.noctalia
     ]
     ++ nflib.scanPaths ./. { };
+    programs.noctalia.settings.lockscreen_widgets.widget = {
+      "lockscreen-login-box@DP-1" = {
+        type = "login_box";
+        output = "DP-1";
+        cx = 1720.0;
+        cy = 1321.0;
+        box_width = 400.0;
+        box_height = 70.0;
+        rotation = 0.0;
+        settings = {
+          layout = "compact";
+          show_media = false;
+        };
+      };
+      "lockscreen-login-box@HDMI-A-2" = {
+        type = "login_box";
+        output = "HDMI-A-2";
+        cx = 960.0;
+        cy = 961.0;
+        box_width = 400.0;
+        box_height = 70.0;
+        rotation = 0.0;
+        settings = {
+          layout = "compact";
+          show_media = false;
+        };
+      };
+    };
   };
 }
--- machines/lpt-titan/home/noctalia.nix
+++ machines/lpt-titan/home/noctalia.nix
@@ -3,4 +3,18 @@ _: {
     lock.timeout = 900;
     suspend.timeout = 1800;
   };
+
+  programs.noctalia.settings.lockscreen_widgets.widget."lockscreen-login-box@eDP-1" = {
+    type = "login_box";
+    output = "eDP-1";
+    cx = 823.0;
+    cy = 978.0;
+    box_width = 400.0;
+    box_height = 70.0;
+    rotation = 0.0;
+    settings = {
+      layout = "compact";
+      show_media = false;
+    };
+  };
 }
