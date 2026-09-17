4c45f7581dede282d80e8a4fe94cb7fc6e903a98
fosskar <117449098+fosskar@users.noreply.github.com>
2026-09-17 17:05:03 +0000

    overlays: format sops-go-builder

--- overlays/sops-go-builder.nix
+++ overlays/sops-go-builder.nix
@@ -1,5 +1,4 @@
-_:
-_final: prev: {
+_: _final: prev: {
   # sops-nix still calls buildGo125Module, which nixpkgs removed as
   # end-of-life; drop this once Mic92/sops-nix moves to a current builder
   buildGo125Module = prev.buildGoModule;
